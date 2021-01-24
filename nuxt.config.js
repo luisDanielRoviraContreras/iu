995b54495ac50b5b01347c7c4ea142142203a990
ldrovira <luisdanielrovira8@gmail.com>
2021-01-24 20:15:04 +0100

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,10 +17,6 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'server',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
   head: {
     title: process.env.npm_package_name || '',
     meta: [
